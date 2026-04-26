31c8c5ee278d85f56f67da86d53855bac44fc2a9
anakin-2004 <anakin.timm@icloud.com>
2026-04-26 15:24:34 +0200

    bugfix to first red test

--- src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -118,6 +118,10 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
+        if (latestOperation == null || latestOperation.isEmpty()) {
+            return;
+        }
+
         var result = switch(latestOperation) {
             case "+" -> latestValue + Double.parseDouble(screen);
             case "-" -> latestValue - Double.parseDouble(screen);
@@ -125,7 +129,9 @@ public class Calculator {
             case "/" -> latestValue / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
+
         screen = Double.toString(result);
+
         if(screen.equals("Infinity")) screen = "Error";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
